baa42e059648c766ee6e7aaec8e8db0f7e8745c5
Ralph <ralph.sleigh@gmail.com>
2023-01-07 19:50:23 +0000

    Fix email sent on approving application

--- src/lambdas/application/approve/handler.ts
+++ src/lambdas/application/approve/handler.ts
@@ -4,8 +4,7 @@ import { lambda_wrapper_json, user, orm } from '../../../lambda-common'
 import { decide_application } from '../../../lambda-common/permissions';
 import { get_user_from_event } from '../../../lambda-common/user';
 import { get_email_client } from '../../../lambda-common/email';
-import * as applicationReceived from '../../../lambda-common/emails/applicationReceived'
-import * as managerApplicationReceived from '../../../lambda-common/emails/managerApplicationReceived'
+import * as applicationApproved from '../../../lambda-common/emails/applicationApproved'
 import { getEventDetails } from '../../../lambda-common/util';
 import { ApplicationModel } from '../../../lambda-common/models/application';
 
@@ -40,7 +39,7 @@ export const lambdaHandler = lambda_wrapper_json([decide_application],
 
         const email = get_email_client(config)
         const values = { event: event, user: user };
-        email.single(user!.email, 'applicationApproved', values);
+        await email.single(user!.email, applicationApproved, values);
 
         return { events: [event] }
 
