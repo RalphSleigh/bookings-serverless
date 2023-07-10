56123d5346fe375de46913f7fcb6557737e8c60f
Ralph <ralph.sleigh@gmail.com>
2023-07-10 16:35:35 +0100

    Fix logging

--- src/lambdas/auth/yahoo/callback/handler.ts
+++ src/lambdas/auth/yahoo/callback/handler.ts
@@ -42,7 +42,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         const profile_response = await fetch(`https://api.login.yahoo.com/openid/v1/userinfo`, {headers: profile_headers})
         const profile = await profile_response.json()
 
-        console.log(JSON.stringify(profile_response))     
+        console.log(JSON.stringify(profile))     
 
         try {
             const user_instance = await user.get_user_from_login(profile.sub, profile.name, profile.email ? profile.email : "", "yahoo")
